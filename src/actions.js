58a7831e6c2e0e16e4a0ddf3b69127a28e1019e1
Olivier Colot <olivier@colot.eu>
2016-12-05 08:42:19 +0100

    actions testing

--- src/actions.js
+++ src/actions.js
@@ -4,42 +4,33 @@ import { createAction as createReduxAction } from 'redux-actions'
 const requestSuffixes = ['request', 'success', 'fail']
 const reducerSuffixes = ['clear', 'clearErrors']
 
-function createType(...array) {
-  return array.map(str => str.toUpperCase()).join('_')
+export const createType = (...array: any) =>
+  array.map(str => str.toUpperCase()).join('_')
+
+export const createRequestType = (collection: string, verb: string, suffix: string) =>
+  createType(collection, verb, suffix)
+
+export const createReducerType = (collection: string, suffix: string) =>
+  createType(collection, suffix)
+
+export const createAction = (type: string) => (payload: any, meta: any) => {
+  const metaCreator = meta ? () => meta : undefined
+  return createReduxAction(type, p => p, metaCreator)(payload)
 }
 
-export function createRequestType(collection: string, verb: string, suffix: string) {
-  return createType(collection, verb, suffix)
-}
-
-export function createReducerType(collection: string, suffix: string) {
-  return createType(collection, suffix)
-}
-
-type RestActionsConfig = { collection: string, verbs: [string] }
-
-function createAction(type) {
-  return (payload, meta) => {
-    const metaCreator = meta ? () => meta : undefined
-    return createReduxAction(type, p => p, metaCreator)(payload)
-  }
-}
-
-export function createRequestActions(collection: string, verb: string) {
-  return requestSuffixes.reduce((result, suffix) => {
+export const createRequestActions = (collection: string, verb: string) =>
+  requestSuffixes.reduce((result, suffix) => {
     const type = createRequestType(collection, verb, suffix)
     result[suffix] = createAction(type)
     return result
   }, {})
-}
 
-export function createReducerActions(collection: string) {
-  return reducerSuffixes.reduce((result, suffix) => {
+export const createReducerActions = (collection: string) =>
+  reducerSuffixes.reduce((result, suffix) => {
     const type = createReducerType(collection, suffix)
     result[suffix] = createAction(type)
     return result
   }, {})
-}
 
 export function createRestActions(config: RestActionsConfig) {
   const { collection, verbs } = config
---
+++ test/actions.spec.js
@@ -0,0 +1,81 @@
+import expect from 'expect'
+
+import {
+  createType,
+  createRequestType,
+  createReducerType,
+  createAction,
+  createRequestActions,
+  createReducerActions,
+  createRestActions,
+} from '../src/actions'
+
+const payload = { pay: 'load'}
+const meta = { meta: 'is it?' }
+
+describe('actions', () => {
+  it('should createType', () => {
+    expect(createType('this', 'Is', 'a', 'TYPE')).toBe('THIS_IS_A_TYPE')
+  })
+
+  it('should createRequestType', () => {
+    expect(createRequestType('cats', 'find', 'request')).toBe('CATS_FIND_REQUEST')
+  })
+
+  it('should createReducerType', () => {
+    expect(createReducerType('cats', 'clear')).toBe('CATS_CLEAR')
+  })
+
+  it('should createAction', () => {
+    const action = createAction('CATS_FIND')
+    expect(typeof action).toBe('function')
+    expect(action(payload, meta)).toEqual({
+      type: 'CATS_FIND',
+      payload,
+      meta,
+    })
+  })
+
+  it('should createRequestActions', () => {
+    const actions = createRequestActions('cats', 'find')
+    for (const key of ['request', 'success', 'fail']) {
+      expect(typeof actions[key]).toBe('function')
+      expect(actions[key](payload, meta)).toEqual({
+        type: `CATS_FIND_${key.toUpperCase()}`,
+        payload,
+        meta,
+      })
+    }
+  })
+
+  it('should createReducerActions', () => {
+    const actions = createReducerActions('cats')
+    expect(actions.clear()).toEqual({
+      type: 'CATS_CLEAR',
+    })
+    expect(actions.clearErrors()).toEqual({
+      type: 'CATS_CLEARERRORS',
+    })
+  })
+
+  it('should createRestActions', () => {
+    const config = {
+      collection: 'cats',
+      verbs: ['find', 'create'],
+    }
+
+    const actions = createRestActions(config)
+
+    for(const verb of config.verbs) {
+      for(const key of ['request', 'success', 'fail']) {
+        expect(typeof actions[verb][key]).toBe('function')
+        expect(actions[verb][key](payload, meta)).toEqual({
+          type: `CATS_${verb}_${key}`.toUpperCase(),
+          payload,
+          meta,
+        })
+      }
+    }
+  })
+
+})
